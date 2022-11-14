5cc1af57989a0ce2c62833a78328a5ec6d93bf2a
mrsamia <15203060@iubat.edu>
2022-11-14 18:30:16 +0600

    SRM | About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,21 +8,23 @@ import Subtitle from '../Components/Subtitle';
 function About(props) {
     return (
         <div>
-              <HeroSection name="About Us"/>
-              <section className='container'>
-              <div className='row as'>
-                    <div className='col-6'>
-                        <h2 className='a'>As a veterinarian and <br></br>lover of animals.</h2>
-                        <Subtitle className='Sub_Title' content="Lorem Ipsum available but the majority have suffered alteration in some form, by injected humour randomised words." />
-                        <OutlineButton name="Our Service" />
-                    </div>
-                    <div className='col-6 d-flex justify-content-end'>
+            <HeroSection name="About Us" />
+            <section className='container'>
+                <div className='row as'>
+                    <div className='col-6  '>
                         <img src={require('../images/img-2.png')} />
                     </div>
+                    <div className='col-6 flex-row-reverse '>
+                        <h2 className='a text-end'>As a veterinarian and <br></br>lover of animals.</h2>
+                        <Subtitle className='Sub_Title text-end' content="Lorem Ipsum available but the majority have suffered alteration in some form, by injected humour randomised words." />
+                        <div className='d-flex justify-content-end'>
+                            <OutlineButton name="Our Service" />
+                        </div>
+                    </div>
                 </div>
-              </section>
-              <HaDeVa/>
-              <Footer/>
+            </section>
+            <HaDeVa />
+            <Footer />
         </div>
     );
 }
